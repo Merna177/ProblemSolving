c8754aa20b0065b263d45e7818ef4a4b1a798f4c
Merna177 <37479454+Merna177@users.noreply.github.com>
2019-07-20 18:46:29 +0200

    solve-->https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/

---
+++ Medium/zigzagLevelOrder.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+       queue<pair<TreeNode*,int> > q;
+        vector<vector<int> > ans;
+        vector<int> path;
+        if(root==NULL){
+           return ans; 
+        }
+        q.push(make_pair(root,0));
+        int prev=0;
+        while(!q.empty()){
+            TreeNode* tp= q.front().first;
+            int level=q.front().second;
+            q.pop();
+          if(tp->left !=NULL)q.push(make_pair(tp->left,level+1));
+           if(tp->right !=NULL)q.push(make_pair(tp->right,level+1));
+            if(level==prev){
+                path.push_back(tp->val);
+                
+            }else{
+                if(prev%2==1)
+                  std::reverse(path.begin(), path.end());
+                prev=level;
+                ans.push_back(path);
+                path.clear();
+                path.push_back(tp->val);
+            }
+           
+
+            
+        }
+        if(prev%2==1)
+            std::reverse(path.begin(), path.end());
+        ans.push_back(path);
+        return ans;
+    }
+};
