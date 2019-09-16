2b48e28d4b68b38bf86e1d8cbbacd99a6666fc97
Merna177 <37479454+Merna177@users.noreply.github.com>
2019-09-16 13:13:52 +0200

    solve-->https://leetcode.com/problems/counting-bits/

--- Medium/countBits.cpp
+++ Medium/countBits.cpp
@@ -9,3 +9,20 @@ public:
         return ans;
     }
 };
+///faster solution
+   public int[] countBits(int num) {
+        int[] res = new int[num+1];
+        int next = 2;
+        int prev = 0;
+        res[0] = 0;
+        for (int i=1; i<=num; i++) {
+            if (i==next) {
+                res[i] = 1;
+                prev = next;
+                next = next*2;
+            } else {
+                res[i] = 1+res[i-prev]; 
+            }
+        }
+        return res; 
+    }
